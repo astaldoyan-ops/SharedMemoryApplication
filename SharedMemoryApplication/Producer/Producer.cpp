893c717be5d35d3cffe1b9fe63430fae0b8c3d2b
Yan <astaldoyan@gmail.com>
2026-07-17 17:57:24 +0300

    Solution created

---
+++ SharedMemoryApplication/Producer/Producer.cpp
@@ -0,0 +1,11 @@
+// Producer.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Producer is running!" << std::endl;
+}
+
+
